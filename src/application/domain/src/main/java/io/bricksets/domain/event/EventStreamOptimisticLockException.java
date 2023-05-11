3561f5a40fa42a20ea1be9f6e76f22b95a34c948
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-11 21:12:04 +0200

    WIP

--- src/application/domain/src/main/java/io/bricksets/domain/event/EventStreamOptimisticLockException.java
+++ src/application/domain/src/main/java/io/bricksets/domain/event/EventStreamOptimisticLockException.java
@@ -4,6 +4,6 @@ import io.bricksets.vocabulary.domain.event.EventId;
 
 public final class EventStreamOptimisticLockException extends RuntimeException {
     public EventStreamOptimisticLockException(EventId lastEventId) {
-        super(String.format("Optimistic lock: state of the aggregate changed since event %s", lastEventId));
+        super(String.format("Optimistic lock: state of the aggregate changed since event with ID %s", lastEventId));
     }
 }
