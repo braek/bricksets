bb2335448f9e751b354e7a7a7b8cdd6af19954b7
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-12 07:54:40 +0200

    WIP

--- src/infrastructure/backend/time-adapter/src/main/java/io/bricksets/system/SystemTimeService.java
+++ src/infrastructure/backend/time-adapter/src/main/java/io/bricksets/system/SystemTimeService.java
@@ -4,10 +4,11 @@ import io.bricksets.domain.time.TimeService;
 import io.bricksets.vocabulary.time.Timestamp;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
 
 public final class SystemTimeService implements TimeService {
     @Override
     public Timestamp now() {
-        return Timestamp.fromInstant(Instant.now());
+        return Timestamp.fromInstant(Instant.now().atOffset(ZoneOffset.UTC).toInstant());
     }
 }
