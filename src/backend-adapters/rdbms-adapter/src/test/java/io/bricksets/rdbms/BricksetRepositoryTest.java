67a5644e17cb6815a179588d6f9d50c890631e90
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-26 00:12:14 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/test/java/io/bricksets/rdbms/BricksetRepositoryTest.java
+++ src/backend-adapters/rdbms-adapter/src/test/java/io/bricksets/rdbms/BricksetRepositoryTest.java
@@ -7,13 +7,11 @@ import io.bricksets.vocabulary.brickset.BricksetTitle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@ContextConfiguration(initializers = {TestContextInitializer.class})
 public class BricksetRepositoryTest {
 
     @Autowired
