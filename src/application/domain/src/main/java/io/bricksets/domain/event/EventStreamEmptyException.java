f9368654feb4c0bce93e44ba9ba764aba97ea2cd
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-25 08:44:16 +0200

    WIP (work in progress)

--- src/application/domain/src/main/java/io/bricksets/domain/event/EventStreamEmptyException.java
+++
@@ -1,7 +0,0 @@
-package io.bricksets.domain.event;
-
-public final class EventStreamEmptyException extends RuntimeException {
-    public EventStreamEmptyException() {
-        super("You cannot build  from an empty EventStream!");
-    }
-}
