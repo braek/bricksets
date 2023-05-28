e04aca54fbb592e4972d201001daffdd37fce86d
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-28 04:06:23 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/RdbmsBricksetRepository.java
+++ src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/RdbmsBricksetRepository.java
@@ -11,8 +11,9 @@ import io.bricksets.vocabulary.brickset.BricksetNumber;
 import org.jooq.DSLContext;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 
@@ -25,24 +26,24 @@ public class RdbmsBricksetRepository extends RdbmsBaseRepository implements Bric
 
     @Override
     public boolean exists(final BricksetNumber number) {
-        var creations = dsl.selectFrom(Tables.EVENT)
+        final Map<BricksetId, BricksetNumber> numbers = new HashMap<>();
+        var events = dsl.selectFrom(Tables.EVENT)
                 .where(Tables.EVENT.EVENT_CLASS.eq(BricksetCreated.class.getSimpleName()))
+                .or(Tables.EVENT.EVENT_CLASS.eq(BricksetRemoved.class.getSimpleName()))
+                .orderBy(Tables.EVENT.POSITION.asc())
                 .fetch()
                 .stream()
                 .map(it -> EventMapper.INSTANCE.map(it, emptyList()))
-                .map(BricksetCreated.class::cast)
-                .map(BricksetCreated::number)
-                .collect(Collectors.toSet());
-        var removals = dsl.selectFrom(Tables.EVENT)
-                .where(Tables.EVENT.EVENT_CLASS.eq(BricksetRemoved.class.getSimpleName()))
-                .fetch()
-                .stream()
-                .map(it -> EventMapper.INSTANCE.map(it, emptyList()))
-                .map(BricksetRemoved.class::cast)
-                .map(BricksetRemoved::number)
-                .collect(Collectors.toSet());
-        creations.removeAll(removals);
-        return creations.contains(number);
+                .toList();
+        events.forEach(event -> {
+            if (event instanceof BricksetCreated created) {
+                numbers.put(created.bricksetId(), created.number());
+            }
+            if (event instanceof BricksetRemoved removed) {
+                numbers.remove(removed.bricksetId());
+            }
+        });
+        return numbers.containsValue(number);
     }
 
     @Override
