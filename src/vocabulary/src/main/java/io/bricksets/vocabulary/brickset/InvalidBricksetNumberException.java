10aa88f51efabefb6617ae4f7c0f418ab8c494df
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-29 21:55:10 +0200

    WIP (work in progress)

--- src/vocabulary/src/main/java/io/bricksets/vocabulary/brickset/InvalidBricksetNumberException.java
+++ src/vocabulary/src/main/java/io/bricksets/vocabulary/brickset/InvalidBricksetNumberException.java
@@ -2,6 +2,6 @@ package io.bricksets.vocabulary.brickset;
 
 public final class InvalidBricksetNumberException extends RuntimeException {
     public InvalidBricksetNumberException(final String str) {
-        super(String.format("Cannot create Brickset Number from string (%s)", str));
+        super(String.format("Cannot create BricksetNumber from string (%s)", str));
     }
 }
