3578cbc4af7d77b2578e4dbf99e9374e86f5d98d
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-29 21:54:48 +0200

    WIP (work in progress)

--- src/vocabulary/src/main/java/io/bricksets/vocabulary/brickset/InvalidBricksetTitleException.java
+++ src/vocabulary/src/main/java/io/bricksets/vocabulary/brickset/InvalidBricksetTitleException.java
@@ -2,6 +2,6 @@ package io.bricksets.vocabulary.brickset;
 
 public final class InvalidBricksetTitleException extends RuntimeException {
     public InvalidBricksetTitleException(String str) {
-        super(String.format("Cannot create Brickset Title from string %s", str));
+        super(String.format("Cannot create Brickset Title from string (%s)", str));
     }
 }
