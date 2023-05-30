72ef8738a76eb4562a2d1db866740fa4073531fa
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-30 17:04:57 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/mapper/CustomObjectMapper.java
+++ src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/mapper/CustomObjectMapper.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.bricksets.domain.event.Event;
-import io.bricksets.vocabulary.brickset.BricksetId;
 import io.bricksets.vocabulary.brickset.BricksetNumber;
 import io.bricksets.vocabulary.brickset.BricksetTitle;
-import io.bricksets.vocabulary.domain.event.EventId;
-import io.bricksets.vocabulary.time.Timestamp;
 
 public final class CustomObjectMapper extends ObjectMapper {
     public CustomObjectMapper() {
@@ -16,11 +13,8 @@ public final class CustomObjectMapper extends ObjectMapper {
         final var bindings = new SimpleModule();
 
         // Serializers
-        bindings.addSerializer(BricksetId.class, new ToStringSerializer());
         bindings.addSerializer(BricksetNumber.class, new ToStringSerializer());
         bindings.addSerializer(BricksetTitle.class, new ToStringSerializer());
-        bindings.addSerializer(EventId.class, new ToStringSerializer());
-        bindings.addSerializer(Timestamp.class, new ToStringSerializer());
 
         // Mixins
         bindings.setMixInAnnotation(Event.class, EventMixin.class);
