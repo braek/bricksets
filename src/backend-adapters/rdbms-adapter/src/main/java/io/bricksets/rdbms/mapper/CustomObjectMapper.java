0a6608d33ca60d4757289634ee0ff8a7c13fd558
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-30 17:04:33 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/mapper/CustomObjectMapper.java
+++ src/backend-adapters/rdbms-adapter/src/main/java/io/bricksets/rdbms/mapper/CustomObjectMapper.java
@@ -1,7 +1,6 @@
 package io.bricksets.rdbms.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.bricksets.domain.event.Event;
@@ -22,9 +21,6 @@ public final class CustomObjectMapper extends ObjectMapper {
         bindings.addSerializer(BricksetTitle.class, new ToStringSerializer());
         bindings.addSerializer(EventId.class, new ToStringSerializer());
         bindings.addSerializer(Timestamp.class, new ToStringSerializer());
-
-        // Deserializers
-        bindings.addDeserializer(BricksetId.class, new BricksetIdDeserializer());
 
         // Mixins
         bindings.setMixInAnnotation(Event.class, EventMixin.class);
