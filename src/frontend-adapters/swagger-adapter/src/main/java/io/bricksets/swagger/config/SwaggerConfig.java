2ab82aa315f7353224ed001ed964c07ace43cee0
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-30 22:36:31 +0200

    WIP (work in progress)

---
+++ src/frontend-adapters/swagger-adapter/src/main/java/io/bricksets/swagger/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package io.bricksets.swagger.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Event Sourcing Sandbox")
+                        .version("dev")
+                )
+                .tags(List.of(
+                        new Tag().name("Bricksets")
+                ));
+    }
+}
