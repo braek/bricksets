b56d406fdd98afcee0f8bdbe712fb4c661380230
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-05-31 21:31:06 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/java/be/koder/bricksets/rdbms/mapper/EventMixin.java
+++ src/backend-adapters/rdbms-adapter/src/main/java/be/koder/bricksets/rdbms/mapper/EventMixin.java
@@ -1,13 +1,14 @@
 package be.koder.bricksets.rdbms.mapper;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import be.koder.bricksets.vocabulary.domain.AggregateId;
 import be.koder.bricksets.vocabulary.domain.event.EventId;
 import be.koder.bricksets.vocabulary.time.Timestamp;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.Set;
 
-// Make sure that ID, occurred on and tags are ignored from Event content, because these are separate columns or tables
+// Make sure that ID, occurred on and tags are ignored from Event content,
+// because these are separate columns or tables
 public abstract class EventMixin {
 
     @JsonIgnore
